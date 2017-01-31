86ec057d8f55ff0abbbcbc1895785aad30feeb25
Logan Fawcett <Logan Fawcett>
2017-01-30 21:05:17 -0600

    Complete Lab 1

---
+++ Lab 1/Lab 1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab_1.Models;
+
+namespace Lab_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        string Date = DateTime.Now.ToString("MM/dd/yyyy");
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Date = Date;
+
+            return View();
+        }
+    }
+}
---
+++ Lab 1/Lab 1/Models/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab_1.Models
+{
+    public class UserInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+    }
+}
