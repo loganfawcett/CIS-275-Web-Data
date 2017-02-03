b039a06863cf9ffb3b7ae5dd0c08b0631b2f9652
svc-student <svc-student@student.directory.jccc>
2017-02-02 19:43:16 -0600

    Added CSS styles to index and form

---
+++ Class 3/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            //Conditional operators
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Evening";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            return View("Thanks", guestResponse);
+        }
+
+    }
+}
