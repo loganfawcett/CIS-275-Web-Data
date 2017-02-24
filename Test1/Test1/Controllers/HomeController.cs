04104c40077457421c011a545c2f098afa88b364
svc-student <svc-student@student.directory.jccc>
2017-02-23 19:12:57 -0600

    Added test1

---
+++ Test1/Test1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test1.Models;
+
+namespace Test1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult carForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult carForm(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Complete", car);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Test1/Test1/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test1.Models
+{
+    public class Car
+    {
+        [Required(ErrorMessage = "Please enter a make.")]
+        public string Make { get; set; }
+        [Required(ErrorMessage = "Please enter a model.")]
+        public string Model { get; set; }
+        [Required(ErrorMessage = "Please enter a price.")]
+        public double Price { get; set; }
+        public int Milage { get; set; }
+        [Required(ErrorMessage = "Please enter a year.")]
+        public int Year { get; set; }
+    }
+}
