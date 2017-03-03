82d8827a84729162b3cbe3a095dc1efa38fe8c4c
Yifei Niu <yniu@stumail.jccc.edu>
2017-03-02 20:08:35 -0600

    Added lab 5

---
+++ Labs/Lab 5/Lab 5/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab_5.Models;
+
+namespace Lab_5.Controllers
+{
+    public class HomeController : Controller
+    {
+        Car[] myCars = {
+                new Car {   Name = "Subaru Outback XT",         Year = 2005,    Cylinders = 4,      Price = 6500.00M},
+                new Car {   Name = "Dodge Viper",               Year = 2003,    Cylinders = 10,     Price = 9000.00M},
+                new Car {   Name = "Nissan GTR",                Year = 2017,    Cylinders = 6,      Price = 120000.00M},
+                new Car {   Name = "Lamborghini Murcielago",    Year = 2005,    Cylinders = 10,     Price = 190000.00M},
+                new Car {   Name = "Ford Shelby GT500",         Year = 2017,    Cylinders = 8,      Price = 53000.00M},
+                new Car {   Name = "Bugatti Veyron",            Year = 2017,    Cylinders = 16,     Price = 2700000.00M}
+            };
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Labs/Lab 5/Lab 5/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab_5.Models
+{
+    public class Car
+    {
+        public int Cylinders { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Year { get; set; }
+    }
+}
