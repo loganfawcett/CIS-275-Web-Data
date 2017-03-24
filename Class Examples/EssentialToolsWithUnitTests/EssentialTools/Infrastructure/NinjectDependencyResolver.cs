60da0e8026b7fe9a7319e8acc06bbc8a4195baaa
svc-student <svc-student@student.directory.jccc>
2017-03-23 19:37:09 -0500

    Base of Unit Testing

---
+++ Class Examples/EssentialToolsWithUnitTests/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+           // kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>();
+            kernel.Bind<IDiscountHelper>()
+                .To<DefaultDiscountHelper>().WithPropertyValue("DiscountSize", 50M);
+            kernel.Bind<IDiscountHelper>().To<FlexibleDiscountHelper>()
+                .WhenInjectedInto<LinqValueCalculator>();
+        }
+    }
+}
---
+++ Class Examples/EssentialToolsWithUnitTests/EssentialTools/Models/DefaultDiscountHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace EssentialTools.Models
+{
+  
+    public class DefaultDiscountHelper : IDiscountHelper
+    {
+
+        public decimal DiscountSize { get; set; }
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            return (totalParam - (DiscountSize / 100m * totalParam));
+        }
+    }
+}
