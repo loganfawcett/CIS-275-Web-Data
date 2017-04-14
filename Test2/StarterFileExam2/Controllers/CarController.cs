458f6a9e35d3b98e3b54e4ba36ecfd8c71c7dcf8
svc-student <svc-student@student.directory.jccc>
2017-04-13 19:29:58 -0500

    Finished Test 2

--- Test2/StarterFileExam2/Controllers/CarController.cs
+++ Test2/StarterFileExam2/Controllers/CarController.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using StarterFileExam2.Models;
+using System.Linq;
+using StarterFileExam2.Infrastructure;
 
 namespace StarterFileExam2.Controllers
 {
     public class CarController : Controller
     {
+        NinjectDependencyResolver res;
         // GET: Car
         public ActionResult InventoryList(string make)
         {
             ICarRepository repository = new EFCarRepository();
-            IEnumerable<Car> Cars = repository.Cars;
+            IEnumerable<Car> Cars = repository.Cars.OrderBy(x => x.Miles);
 
             return View(Cars);
 
