1e310ec463113012cff7af53e987a7bf934d12bd
svc-student <svc-student@student.directory.jccc>
2017-04-27 20:07:28 -0500

    Added lab 10

---
+++ Labs/Lab 10/MovieLab/Models/EFMovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieLab.Models;
+
+namespace MovieLab.Models
+{
+    public class EFMovieRepository : IMovieRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IEnumerable<Movie> Movies
+        {
+            get { return context.Movies; }
+        }
+              
+    }
+}
