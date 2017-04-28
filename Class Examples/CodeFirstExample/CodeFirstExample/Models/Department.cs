e7df60025c576760563ea8649933c9c656863469
svc-student <svc-student@student.directory.jccc>
2017-04-27 20:05:49 -0500

    Finished in class example

---
+++ Class Examples/CodeFirstExample/CodeFirstExample/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstExample.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        [DisplayName("Department")]
+        public string Name { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
---
+++ Class Examples/CodeFirstExample/CodeFirstExample/Models/Initializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace CodeFirstExample.Models
+{
+    public class Initializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<CodeFirstExampleContext>
+    {
+        protected override void Seed(CodeFirstExampleContext context)
+        {
+            var departments = new List<Department>
+                {
+                new Department{Name="Mathematics"},
+                new Department{Name="Computer Science"},
+                new Department{Name="History"},
+                new Department{Name="English"}
+                };
+            departments.ForEach(d => context.Departments.Add(d));
+            context.SaveChanges();
+            var courses = new List<Course>
+                {
+                new Course{DepartmentID=1,Title="College Algebra"},
+                new Course{DepartmentID=4,Title="English Comp I"},
+                new Course{DepartmentID=2,Title="Programming Fundamentals"}
+                };
+            courses.ForEach(c => context.Courses.Add(c));
+            context.SaveChanges();
+        }
+    }
+}
